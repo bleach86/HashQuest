b42e671c64c74dcbdf64da47cc8b0faea9c990b5
bleach86 <14421739+bleach86@users.noreply.github.com>
2024-08-18 17:00:25 -0500

    correct upgrade cost

--- src/market.rs
+++ src/market.rs
@@ -78,9 +78,7 @@ pub struct Bank {
 
 impl Bank {
     pub fn new() -> Self {
-        Bank {
-            balance: 10000000.0,
-        }
+        Bank { balance: 0.0 }
     }
 
     pub fn deposit(&mut self, amount: f64) {
